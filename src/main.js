81e39a6878f1620533c58f1af83f65b5c8c39d7f
George Khella <gkh36@canterbury.ac.nz>
2019-05-03 12:33:06 +1200

    Added basic folders and files

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import App from './App.vue';
+import  Home  from  './Home.vue';
+import  Users  from  './Users.vue';
+
+import VueRouter from 'vue‑router';
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: Home
+  },
+  {
+    path: "/users",
+    component: Users
+  }
+];
+
+const router = new VueRouter ({
+  routes: routes,
+  mode: 'history'
+});
+
+new Vue({
+  el: '#app',
+  router: router,
+  render: h => h(App)
+});

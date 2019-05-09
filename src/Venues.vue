26fff35d39cdf52e91d3ba3691efeed6cb8c223e
George K <gkh36@uclive.ac.nz>
2019-05-09 23:51:24 +1200

    Search Venues by city, added menu

--- src/Venues.vue
+++ src/Venues.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <!--Search venues button-->
-    <button type="button" class="btn btn-primary" v-on:click="getVenues()">
-      Search
-    </button>
+    <Menu/>
+
+
+    <div id="search-wrapper">
+      <input type="text" v-model="search" placeholder="City"/>
+      <!--Search venues button-->
+      <button type="button" class="btn btn-primary" v-on:click="getVenues()">
+        Search
+      </button>
+    </div>
+
     <div id="venues">
       <table>
         <tr>
@@ -13,7 +20,7 @@
         </tr>
         <tr v-for="venue in venues">
           <td> {{ venue.venueName }} <br></td>
-          <td> {{ venue.categoryId }} <br></td>
+          <td> {{ getCategoryName(venue.categoryId) }} <br></td>
           <td>
             <img :src="getVenuePrimaryPhoto(venue.venueId,venue.primaryPhoto)" width="100px">
             <br>
@@ -29,26 +36,43 @@
 </template>
 
 <script>
+  import Menu from "./Menu";
   export default {
     data() {
       return {
         error: "",
         errorFlag: false,
-        venues: []
+        venues: [],
+        categories: [],
+        search: ""
       }
     },
     mounted: function () {
-      // this.getVenues();
+      this.getVenueCategory();
+    },
+    computed: {
+
     },
     methods: {
       getVenues: function () {
-        this.$http.get("http://localhost:4941/api/v1/venues")
+        this.$http.get("http://localhost:4941/api/v1/venues?city=" + this.search)
           .then(function (response) {
             this.venues = response.data;
-          }, function (error) {
+          }, function(error) {
             this.error = error;
             this.errorFlag = true;
+          })
+      },
+      getVenueCategory: function() {
+        this.$http.get("http://localhost:4941/api/v1/categories")
+          .then(function (response) {
+            this.categories = response.data;
           });
+      },
+      getCategoryName: function(id) {
+        if (typeof(this.categories[id-1]) !== "undefined") {
+          return this.categories[id - 1].categoryName;
+        }
       },
       getVenuePrimaryPhoto: function (id, filename) {
         if (filename === null) {
@@ -56,7 +80,11 @@
         }
         return "http://localhost:4941/api/v1/venues/" + id + "/photos/" + filename
       }
+    },
+    components: {
+      Menu
     }
+
   }
 </script>
 

8df57d474233ad693435c4a802bab5a3f1669236
George K <gkh36@uclive.ac.nz>
2019-05-13 23:33:58 +1200

    Added div

--- src/Menu.vue
+++ src/Menu.vue
@@ -1,49 +1,52 @@
 <template>
-  <v-layout>
-    <v-toolbar id="NavMenu" fixed>
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Swoosh</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link One</v-btn>
-        <v-btn flat>Link Two</v-btn>
-        <v-btn flat>Link Three</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+  <div>
+    <v-layout>
+      <v-toolbar id="NavMenu" fixed>
+        <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Swoosh</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>Link One</v-btn>
+          <v-btn flat>Link Two</v-btn>
+          <v-btn flat>Link Three</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
 
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list class="pa-1">
-        <v-list-tile avatar>
-          <v-list-tile-avatar>
-            <!--TOOD: Add user's profile photo or default pic -->
-            <v-icon>person</v-icon>
-          </v-list-tile-avatar>
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list class="pa-1">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <!--TOOD: Add user's profile photo or default pic -->
+              <v-icon>person</v-icon>
+            </v-list-tile-avatar>
 
-          <v-list-tile-content>
-            <v-list-tile-title>
-              <!--TODO: Add user's name here or sign in-->
-              Sign In
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                <!--TODO: Add user's name here or sign in-->
+                <router-link :to="{name: 'login'}">Sign In</router-link>
 
-      <v-list class="pt-0" dense>
-        <v-divider></v-divider>
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
 
-        <v-list-tile v-for="item in items" :key="item.title" @click="" :to="item.route">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
+        <v-list class="pt-0" dense>
+          <v-divider></v-divider>
 
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+          <v-list-tile v-for="item in items" :key="item.title" :to="item.route">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
 
-      </v-list>
-    </v-navigation-drawer>
-  </v-layout>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+        </v-list>
+      </v-navigation-drawer>
+    </v-layout>
+  </div>
 </template>
 
 
@@ -52,11 +55,11 @@
     name: "Menu",
     data() {
       return {
-        drawer: null,
+        drawer: false,
         items: [
-          { title: 'Home', icon: 'home', route: '/' },
-          { title: 'Venues', icon: 'place', route: 'venues'},
-          { title: 'logout', icon: 'exit_to_app', route: '/'} //TODO: Add log out function call here
+          {title: 'Home', icon: 'home', route: '/'},
+          {title: 'Venues', icon: 'place', route: 'venues'},
+          {title: 'logout', icon: 'exit_to_app', route: '/'} //TODO: Add log out function call here
         ]
       }
     }

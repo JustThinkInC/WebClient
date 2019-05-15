27f80aa832b066d49aaf1a5cc4ce0812d436c324
George K <gkh36@uclive.ac.nz>
2019-05-16 08:49:34 +1200

    Correct lastName to familyName

--- src/Profile.vue
+++ src/Profile.vue
@@ -57,7 +57,7 @@
               <v-list-tile-action></v-list-tile-action>
 
               <v-list-tile-content>
-                <v-list-tile-title>{{currentUser.lastName}}</v-list-tile-title>
+                <v-list-tile-title>{{currentUser.familyName}}</v-list-tile-title>
                 <v-list-tile-sub-title>Last name</v-list-tile-sub-title>
               </v-list-tile-content>
             </v-list-tile>

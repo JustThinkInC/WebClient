97f76ccc00692674e1411988129a2b5123ce6a09
George K <gkh36@uclive.ac.nz>
2019-05-20 12:40:10 +1200

    Add title, sub title and carousel to home page
    Images used are placeholders

--- src/Home.vue
+++ src/Home.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
+  <v-app>
     <Menu/>
-  </div>
+
+    <v-container align-center mt-5 justify-center>
+      <v-layout align-center justify-center column>
+        <h1>Swoosh</h1>
+        <h2>Your venue manager</h2>
+        <v-carousel>
+          <v-carousel-item src="src/assets/defaultProfile.jpg"></v-carousel-item>
+          <v-carousel-item src="src/assets/logo.png"></v-carousel-item>
+          <v-carousel-item src="src/assets/defaultProfile.png"></v-carousel-item>
+        </v-carousel>
+      </v-layout>
+    </v-container>
+
+  </v-app>
 </template>
 
 <script>
